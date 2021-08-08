dd4b4f61ff84015955b3ebbcc4217306395e8547
Artem <artem15052003@gmail.com>
2021-08-08 16:03:45 +0300

    update console output method

--- js/lesson21/search-element/index.js
+++ js/lesson21/search-element/index.js
@@ -1,11 +1,11 @@
 export function getTitleElement(selector = ".title") {
 	const titleElement = document.querySelector(selector);
-	console.log(titleElement);
+	console.dir(titleElement);
 	return titleElement;
 }
 
 export function getInputElement(selector = 'input[type="text"]') {
 	const inputElement = document.querySelector(selector);
-	console.log(inputElement);
+	console.dir(inputElement);
 	return inputElement;
 }
