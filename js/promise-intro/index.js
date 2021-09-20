2c717d11dc8932042254bf321ef7e441f0efc6d6
Artem <artem15052003@gmail.com>
2021-09-20 10:57:36 +0300

    added usage of addImage to Promise

--- js/promise-intro/index.js
+++ js/promise-intro/index.js
@@ -1,20 +1,15 @@
-export const addImageV2 = url => 
-    new Promise((resolve, reject) => {
-        const imgElem = document.createElement('img');
-        imgElem.alt = 'Image';
-        imgElem.src = url;
-        document.querySelector('.page').append(imgElem);
-        document.addEventListener('load', () => {
-          const { width, height } = imgElem;
-          resolve({ width, height });
-        });
-        document.addEventListener('error', () => 
-          reject('Image load failed')
-        );
-    }); 
+import { addImage } from "./addImage";
+
+export const addImageV2 = (url) =>
+	new Promise((resolve, reject) => {
+		addImage(url, (error, data) => {
+			if (error) reject(error);
+			else resolve(data);
+		});
+	});
 
 // examples
 
-addImageV2('https://server.com/image.png')
-  .then(data => console.log(data)) // ==> { width: 200, height: 100 }
-  .catch(error => console.log(error)); // ==> 'Image load failed'
+addImageV2("https://server.com/image.png")
+	.then((data) => console.log(data)) // ==> { width: 200, height: 100 }
+	.catch((error) => console.log(error)); // ==> 'Image load failed'
