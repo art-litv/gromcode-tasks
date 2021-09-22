e0d91f8f6d3d183965f28a275910ede7f884662d
Artem <artem15052003@gmail.com>
2021-09-22 18:42:56 +0300

    fetch task

---
+++ js/http/fetch/index.js
@@ -0,0 +1,19 @@
+const fetchUserData = (userName) => {
+	const apiUrl = "https://api.github.com/users/";
+	return fetch(apiUrl + userName)
+		.then((data) => data.json())
+		.then((jsonData) => jsonData);
+};
+
+const renderUserData = (userName) => {
+	fetchUserData(userName).then((data) => {
+		document.querySelector(".user__avatar").src = data.avatar_url;
+		document.querySelector(".user__name").textContent = data.login;
+		document.querySelector(".user__location").textContent = data.location || "";
+	});
+};
+
+document.querySelector(".btn").addEventListener("click", () => {
+	const userNameInputElem = document.querySelector(".name-form__input");
+	renderUserData(userNameInputElem.value);
+});
