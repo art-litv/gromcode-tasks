e32b39093d04cf2726525f3b37ba29268a57a80a
Artem <artem15052003@gmail.com>
2021-09-22 19:07:05 +0300

    changed fetch logic

--- js/http/fetch/index.js
+++ js/http/fetch/index.js
@@ -2,15 +2,20 @@ const userAvatarElem = document.querySelector(".user__avatar");
 const userNameElem = document.querySelector(".user__name");
 const userLocationElem = document.querySelector(".user__location");
 
+const defaultAvatarUrl = "https://avatars3.githubusercontent.com/b10001";
+userAvatarElem.src = defaultAvatarUrl;
+
 const fetchUserData = (userName) => {
 	const apiUrl = "https://api.github.com/users/";
-	return fetch(apiUrl + userName).then((response) => response.json());
+	return fetch(apiUrl + userName).then((response) =>
+		response.ok ? response.json() : { avatar_url: defaultAvatarUrl }
+	);
 };
 
 const renderUserData = (userData) => {
 	const { avatar_url, login, location } = userData;
 	userAvatarElem.src = avatar_url;
-	userNameElem.textContent = login;
+	userNameElem.textContent = login || "";
 	userLocationElem.textContent = location ? `from ${location}` : "";
 };
 
