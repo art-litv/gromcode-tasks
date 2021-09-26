274b5ea2c81df2a9e7c4fb309353639452e2eeee
Artem <artem15052003@gmail.com>
2021-09-26 17:06:45 +0300

    changed login destructurization to name

--- js/http/fetch/index.js
+++ js/http/fetch/index.js
@@ -1,33 +1,38 @@
+const fetchUserData = (userId) => {
+	return fetch(`https://api.github.com/users/${userId}`).then((response) =>
+		response.json()
+	);
+};
 const userAvatarElem = document.querySelector(".user__avatar");
 const userNameElem = document.querySelector(".user__name");
 const userLocationElem = document.querySelector(".user__location");
 
-const defaultAvatarUrl =
-	"https://image-control-storage.s3.amazonaws.com/github-512.png";
+const defaultAvatar = "https://avatars3.githubusercontent.com/u100011";
 
-userAvatarElem.src = defaultAvatarUrl;
-
-const fetchUserData = (userName) => {
-	const apiUrl = "https://api.github.com/users/";
-	return fetch(apiUrl + userName).then((response) =>
-		response.ok ? response.json() : { avatar_url: defaultAvatarUrl }
-	);
+const setDefaultAvatar = () => {
+	if (userAvatarElem.src !== defaultAvatar) {
+		userAvatarElem.src = defaultAvatar;
+	}
 };
 
-const renderUserData = (userData) => {
-	const { avatar_url, login, location } = userData;
+setDefaultAvatar();
+
+userAvatarElem.addEventListener("error", setDefaultAvatar);
+
+const renderUser = (userData) => {
+	const { avatar_url, name, location } = userData;
+
 	userAvatarElem.src = avatar_url;
-	userNameElem.textContent = login || "";
+	userNameElem.textContent = name;
 	userLocationElem.textContent = location ? `from ${location}` : "";
 };
 
+const showUserBtnElem = document.querySelector(".name-form__btn");
+const userNameInputElem = document.querySelector(".name-form__input");
+
 const onSearchUser = () => {
-	const userNameInputElem = document.querySelector(".name-form__input");
-	fetchUserData(userNameInputElem.value).then((userData) =>
-		renderUserData(userData)
-	);
+	const userld = userNameInputElem.value;
+	fetchUserData(userld).then((userData) => renderUser(userData));
 };
 
-document
-	.querySelector(".name-form__btn")
-	.addEventListener("click", onSearchUser);
+showUserBtnElem.addEventListener("click", onSearchUser);
