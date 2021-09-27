d17ae3575d2e60057298017e4715675eee65cedb
Artem <artem15052003@gmail.com>
2021-09-27 18:11:56 +0300

    integrated REST API

---
+++ js/http-complex/todo-list/src/scripts/tasks/createTask.js
@@ -0,0 +1,24 @@
+import { renderTasks } from "../common/renderer.js";
+import { getItem, setItem } from "../common/storage.js";
+import { createTask, getTasksList } from "../common/tasksGateway.js";
+
+const createTaskBtnElem = document.querySelector(".create-task-btn");
+
+const onCreateBtnClick = () => {
+	getTasksList().then((tasks) => {
+		const newTaskElement = document.querySelector(".task-input");
+		if (!newTaskElement.value) return;
+
+		const id = tasks.length ? Math.max(...tasks.map((task) => task.id)) + 1 : 1;
+		const text = newTaskElement.value;
+		createTask({ id, text, isDone: false })
+			.then(getTasksList)
+			.then((newTasksList) => {
+				renderTasks(newTasksList);
+			});
+
+		newTaskElement.value = "";
+	});
+};
+
+createTaskBtnElem.addEventListener("click", onCreateBtnClick);
