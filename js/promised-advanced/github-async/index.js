afa765524fbac240c32467fb59c0a1255b984291
Artem <artem15052003@gmail.com>
2021-09-30 13:46:29 +0300

    remade promises to async syntax

---
+++ js/promised-advanced/github-async/index.js
@@ -0,0 +1,69 @@
+const fetchUserData = async (userId) => {
+	const response = await fetch(`https://api.github.com/users/${userId}`);
+	return await response.json();
+};
+
+const fetchRepos = async (reposUrl) => {
+	const response = await fetch(reposUrl);
+	return await response.json();
+};
+
+const userAvatarElem = document.querySelector(".user__avatar");
+const userNameElem = document.querySelector(".user__name");
+const userLocationElem = document.querySelector(".user__location");
+const repoListElem = document.querySelector(".repo-list");
+const spinnerElem = document.querySelector(".spinner");
+
+const defaultAvatar = "https://avatars3.githubusercontent.com/u10001";
+
+const setDefaultAvatar = () => {
+	if (userAvatarElem.src !== defaultAvatar) {
+		userAvatarElem.src = defaultAvatar;
+	}
+};
+
+setDefaultAvatar();
+
+userAvatarElem.addEventListener("error", setDefaultAvatar);
+
+const renderUser = (userData) => {
+	const { avatar_url, name, location } = userData;
+
+	userAvatarElem.src = avatar_url;
+	userNameElem.textContent = name;
+	userLocationElem.textContent = location ? `from ${location}` : "";
+};
+
+const renderUserRepos = (reposData) => {
+	repoListElem.innerHTML = "";
+	const repoElemsArray = reposData.map(({ name }) => {
+		const repoElem = document.createElement("li");
+		repoElem.classList.add("repo-list__item");
+		repoElem.textContent = name;
+		return repoElem;
+	});
+
+	repoListElem.append(...repoElemsArray);
+};
+
+const showUserBtnElem = document.querySelector(".name-form__btn");
+const userNameInputElem = document.querySelector(".name-form__input");
+
+const onSearchUser = async () => {
+	const userId = userNameInputElem.value;
+
+	spinnerElem.classList.remove("spinner_hidden");
+	try {
+		const { repos_url } = userData;
+		const userData = await fetchUserData(userId);
+		const reposData = await fetchRepos(repos_url);
+		renderUser(userData);
+		renderUserRepos(reposData);
+	} catch (err) {
+		alert("Failed to load data");
+	} finally {
+		spinnerElem.classList.add("spinner_hidden");
+	}
+};
+
+showUserBtnElem.addEventListener("click", onSearchUser);
