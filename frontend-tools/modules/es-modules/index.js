3d083370e4f6b48fadfa2f37b6298b37032d56ac
Artem <artem15052003@gmail.com>
2021-10-13 13:59:41 +0300

    es-modules

---
+++ frontend-tools/modules/es-modules/index.js
@@ -0,0 +1,10 @@
+import "./polyfills/array-flat.js";
+import { calc as calcExpression, sum, mult } from "./calculator/index.js";
+import fetchUser from "./profile/gateway.js";
+import { printProfile } from "./profile/index.js";
+
+const calcResult = calcExpression("1 + 2");
+const sumResult = sum(1, 2);
+const multResult = mult(1, 2);
+const userDataPromise = fetchUser("facebook");
+printProfile({ name: "Tom", from: "The World" });
