e41a04cc30e581884a8381eeba87a0171ac20410
Artem <artem15052003@gmail.com>
2021-10-23 23:34:28 +0300

    nested-react

---
+++ react/react-intro/nested-elements-react/index.js
@@ -0,0 +1,22 @@
+const rootElement = document.querySelector("#root");
+
+const greetingElem = React.createElement(
+  "div",
+  { className: "greeting" },
+  React.createElement(
+    "p",
+    {
+      className: "greeting__title",
+    },
+    "Hello, World!"
+  ),
+  React.createElement(
+    "p",
+    {
+      className: "greeting__text",
+    },
+    "I'm learning React"
+  )
+);
+
+ReactDOM.render(greetingElem, rootElement);
