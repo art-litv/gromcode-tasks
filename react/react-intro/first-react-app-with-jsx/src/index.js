cbc6b0ed76d8f76e9d120cb32888aad45ea7da72
Artem <artem15052003@gmail.com>
2021-10-25 13:09:53 +0300

    rewrote jsx

--- react/react-intro/first-react-app-with-jsx/src/index.js
+++ react/react-intro/first-react-app-with-jsx/src/index.js
@@ -7,7 +7,7 @@ const rootElement = document.querySelector("#root");
 const greetingElem = (
   <div className="greeting">
     <h1 className="greeting__title">Hello, world!</h1>
-    <p className="greeting_text">I'm learning React</p>
+    <p className="greeting__text">I'm learning React</p>
   </div>
 );
 
