4f362ef760798870150db6a894f0309dfce9ceaf
Artem <artem15052003@gmail.com>
2021-10-25 14:03:03 +0300

    react-todo-list-init

---
+++ react/react-intro/todo-list-react-init/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+const titleElem = <h1 class="title">Todo List</h1>;
+
+ReactDOM.render(titleElem, document.getElementById("root"));
