8a3ad24bcb09cfdba26dae2070bbc642fd168cac
Artem <artem15052003@gmail.com>
2021-10-26 17:06:27 +0300

    made name as a prop

--- react/components/functional-components/src/Search.jsx
+++ react/components/functional-components/src/Search.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Search() {
+  const { name } = this.props;
+
   return (
     <div className="search">
-      <h1 className="search__title">Hello, Tom. What to search for you?</h1>
+      <h1 className="search__title">Hello, {name}. What to search for you?</h1>
       <div className="search__field">
         <input type="text" className="search__input" />
         <button className="search__button">Search</button>
