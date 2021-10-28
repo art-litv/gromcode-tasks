e4d627217a72cc235190e1a80898317146c728d6
Artem <artem15052003@gmail.com>
2021-10-28 14:19:26 +0300

    made another default export

--- react/events/counter/src/Counter.jsx
+++ react/events/counter/src/Counter.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Counter extends Component {
+class Counter extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -48,3 +48,5 @@ export default class Counter extends Component {
     );
   }
 }
+
+export default Counter;
