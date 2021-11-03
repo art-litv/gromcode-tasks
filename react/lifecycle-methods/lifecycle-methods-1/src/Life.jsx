5747b50378d3004252c1e7394cf55a7b3457e962
Artem <artem15052003@gmail.com>
2021-11-03 11:45:36 +0200

    added state

--- react/lifecycle-methods/lifecycle-methods-1/src/Life.jsx
+++ react/lifecycle-methods/lifecycle-methods-1/src/Life.jsx
@@ -3,15 +3,20 @@ import React from 'react';
 export default class Life extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      state: 0,
+    };
     console.log('constructor: good place to create state');
   }
 
   componentDidMount() {
     console.log('componentDidMount: API calls, subscriptions');
+    this.setState({ state: 1 });
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+    return true;
   }
 
   componentDidUpdate(prevProps, prevState) {
