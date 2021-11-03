b05dad8de0d53e2bee15d0796ee1fdbc377d9209
Artem <artem15052003@gmail.com>
2021-11-03 13:08:34 +0200

    user

---
+++ react/lifecycle-methods/user/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import User from './User.jsx';
+
+const App = () => {
+  return (
+    <div className="App">
+      <User userId="facebook" />
+    </div>
+  );
+};
+
+export default App;
