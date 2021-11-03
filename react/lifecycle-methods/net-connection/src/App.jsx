d55ee9abe3ff3cc89d1d90ddc4bebecf05540246
Artem <artem15052003@gmail.com>
2021-11-03 15:26:47 +0200

    status

---
+++ react/lifecycle-methods/net-connection/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ConnectionStatus from './ConnectionStatus.jsx';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ConnectionStatus />
+    </div>
+  );
+};
+
+export default App;
---
+++ react/lifecycle-methods/net-connection/src/ConnectionStatus.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default class ConnectionStatus extends React.Component {
+  state = {
+    online: true,
+  };
+
+  componentDidMount() {
+    window.addEventListener('online', () => this.setStatus(true));
+    window.addEventListener('offline', () => this.setStatus(false));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', () => this.setStatus(true));
+    window.removeEventListener('offline', () => this.setStatus(false));
+  }
+
+  setStatus(isOnline) {
+    console.log(isOnline);
+    this.setState({ online: isOnline });
+  }
+
+  render() {
+    const statusText = this.state.online ? 'Online' : 'Offline';
+    const statusStyles = `status ${this.state.online ? '' : 'status_offline'}`;
+    return <div className={statusStyles}>{statusText}</div>;
+  }
+}
