419fa8ae99f14a323dd0158d07302a8eb07c8869
Artem <artem15052003@gmail.com>
2021-11-03 11:32:39 +0200

    lifecycle-methods

---
+++ react/lifecycle-methods/lifecycle-methods-1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Life from './Life.jsx';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Life />
+    </div>
+  );
+};
+
+export default App;
