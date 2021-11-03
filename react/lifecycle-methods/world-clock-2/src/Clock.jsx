b36d3d3c01707b332fb68c491405152d0f65f7c7
Artem <artem15052003@gmail.com>
2021-11-03 12:35:59 +0200

    time

--- react/lifecycle-methods/world-clock-2/src/Clock.jsx
+++ react/lifecycle-methods/world-clock-2/src/Clock.jsx
@@ -4,20 +4,32 @@ export default class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: new Date(),
+      date: this.getTimeWithOffset(this.props.offset),
     };
   }
 
   componentDidMount() {
-    this.interval = setInterval(() => this.setState({ date: new Date() }), 1000);
+    this.interval = setInterval(
+      () =>
+        this.setState(() => ({
+          date: this.getTimeWithOffset(this.props.offset),
+        })),
+      1000,
+    );
   }
 
   componentWillUnmount() {
     clearInterval(this.interval);
   }
 
+  getTimeWithOffset = offset => {
+    const currentTime = new Date();
+    const utcOffset = currentTime.getTimezoneOffset() / 60;
+    return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+  };
+
   render() {
-    const { location, offset } = this.props;
+    const { location } = this.props;
     return (
       <div className="clock">
         <div className="clock__location">{location}</div>
