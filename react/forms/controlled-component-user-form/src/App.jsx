f222945015e342914176d096a97ececc4f364c55
Artem <artem15052003@gmail.com>
2021-11-04 12:04:26 +0200

    changed logging

--- react/forms/controlled-component-user-form/src/App.jsx
+++ react/forms/controlled-component-user-form/src/App.jsx
@@ -3,14 +3,8 @@ import React from 'react';
 import UserForm from './UserForm.jsx';
 
 export default class App extends React.Component {
-  createUser = event => {
-    event.preventDefault();
-    console.log(
-      [...new FormData(event.target)].reduce(
-        (acc, [name, value]) => ({ ...acc, [name]: value }),
-        {},
-      ),
-    );
+  createUser = user => {
+    console.log(user);
   };
 
   render() {
--- react/forms/controlled-component-user-form/src/UserForm.jsx
+++ react/forms/controlled-component-user-form/src/UserForm.jsx
@@ -21,7 +21,13 @@ export default class UserForm extends React.Component {
 
   render() {
     return (
-      <form className="login-form" onSubmit={this.props.onSubmit}>
+      <form
+        className="login-form"
+        onSubmit={event => {
+          event.preventDefault();
+          this.props.onSubmit(this.state);
+        }}
+      >
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">
