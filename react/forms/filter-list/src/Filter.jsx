ea885719066843c4a9f54a45e61b37f38935d861
Artem <artem15052003@gmail.com>
2021-11-04 13:09:06 +0200

    userList

---
+++ react/forms/filter-list/src/Filter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Filter({ filterText, count, onChange }) {
+  return (
+    <div className="filter">
+      <span className="filter__count">{count}</span>
+      <input type="text" className="filter__input" onChange={onChange} value={filterText} />
+    </div>
+  );
+}
---
+++ react/forms/filter-list/src/UserList.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import User from './User.jsx';
+import Filter from './Filter.jsx';
+
+export default class UserList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      count: props.users.length,
+    };
+  }
+
+  onChange = ({ target }) => {
+    const filterText = target.value;
+    const count = this.getFilteredUsers(filterText).length;
+    this.setState({ filterText, count });
+  };
+
+  getFilteredUsers = matchName => {
+    return this.props.users.filter(({ name }) => name.includes(matchName));
+  };
+
+  render() {
+    const { filterText } = this.state;
+    const users = filterText.length ? this.getFilteredUsers(filterText) : this.props.users;
+    return (
+      <>
+        <Filter onChange={this.onChange} {...this.state} />
+        <ul className="users">
+          {users.map(({ id, ...userData }) => (
+            <User key={id} {...userData} />
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
