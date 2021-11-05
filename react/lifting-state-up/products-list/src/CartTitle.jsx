a02f59d361944b2a1c096971a620cb928e7602fe
Artem <artem15052003@gmail.com>
2021-11-05 17:23:41 +0200

    renamed component name

--- react/lifting-state-up/products-list/src/CartTitle.jsx
+++ react/lifting-state-up/products-list/src/CartTitle.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function CardTitle({ count, userName }) {
+export default function CartTitle({ count, userName }) {
   return (
     <div className="cart-title">
       {userName}, you added {count} items
