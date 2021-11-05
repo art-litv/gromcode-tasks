8f111f4e233d32089f615fea99b190ce9c4d2fef
Artem <artem15052003@gmail.com>
2021-11-05 17:49:13 +0200

    temperature

---
+++ react/lifting-state-up/water-temp-calc/src/TemperatureInput.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit',
+  };
+
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
+
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {this.scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
