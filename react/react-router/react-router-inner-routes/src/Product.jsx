697f1606de1af9a9974ad6faa075e96f523dc579
Artem <artem15052003@gmail.com>
2021-11-12 20:48:02 +0200

    sdg

--- react/react-router/react-router-inner-routes/src/Product.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-export default function Product() {
-  const { productId } = useParams();
-  return <div class="product">{productId}</div>;
-}
