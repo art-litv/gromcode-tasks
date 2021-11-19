3a4cd18a030e79a2832eeb88f88d9fba6dd6824b
art-litv <artem15052003@gmail.com>
2021-11-19 20:47:32 +0200

    users

--- react/react-router/react-router-inner-routes-users/src/App.jsx
+++ react/react-router/react-router-inner-routes-users/src/App.jsx
@@ -7,18 +7,20 @@ import Users from "./Users.jsx";
 export default function App() {
   return (
     <BrowserRouter>
-      <ul class="navigation">
-        <li class="navigation__item">
-          <Link to="/">Home</Link>
-        </li>
-        <li class="navigation__item">
-          <Link to="/users">Users</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/users" component={Users} />
-      </Switch>
+      <div class="page">
+        <ul class="navigation">
+          <li class="navigation__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li class="navigation__item">
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/users" component={Users} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
