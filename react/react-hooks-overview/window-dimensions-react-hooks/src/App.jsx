257b6cc6a27208ac990e99069d5bf38ce4db5dcb
art-litv <artem15052003@gmail.com>
2021-11-21 18:26:20 +0200

    changed dimensions in app

--- react/react-hooks-overview/window-dimensions-react-hooks/src/App.jsx
+++ react/react-hooks-overview/window-dimensions-react-hooks/src/App.jsx
@@ -1,30 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
-import User from "./User.jsx";
+import Dimensions from "./Dimensions";
 
 export default function App() {
-  return (
-    <div className="page">
-      <BrowserRouter>
-        <div className="page__content">
-          <h1>Users</h1>
-          <ul className="navigation">
-            <li className="navigation__item">
-              <Link to="/users/github">Github</Link>
-            </li>
-            <li className="navigation__item">
-              <Link to="/users/facebook">Facebook</Link>
-            </li>
-          </ul>
-          <Switch>
-            <Route exact path="/">
-              <span>Select a user please</span>
-            </Route>
-            <Route path="/users/:userId" component={User} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+  return <Dimensions />;
 }
