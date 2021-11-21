f3513276c61c438f4dacebf3d56384a4b3586d1d
art-litv <artem15052003@gmail.com>
2021-11-21 17:27:32 +0200

    changed markup

--- react/react-hooks-overview/user-data-react-hooks/src/App.jsx
+++ react/react-hooks-overview/user-data-react-hooks/src/App.jsx
@@ -17,10 +17,13 @@ export default function App() {
               <Link to="/users/facebook">Facebook</Link>
             </li>
           </ul>
+          <Switch>
+            <Route exact path="/users">
+              <span>Select a user please</span>
+            </Route>
+            <Route path="/users/:userId" component={User} />
+          </Switch>
         </div>
-        <Switch>
-          <Route path="/users/:userId" component={User} />
-        </Switch>
       </BrowserRouter>
     </div>
   );
