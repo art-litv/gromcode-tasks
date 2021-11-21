e6b5c0f4d54123dfe165da8935cc5847c3a216c2
art-litv <artem15052003@gmail.com>
2021-11-21 18:21:56 +0200

    dimensions

---
+++ react/react-hooks-overview/window-dimensions-react-hooks/src/Dimensions.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+
+export default function Dimensions() {
+  const [size, setSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    function handleResize() {
+      setSize({ width: window.innerWidth, height: window.innerHeight });
+    }
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div class="dimensions">
+      {size.width}px - {size.height}px
+    </div>
+  );
+}
