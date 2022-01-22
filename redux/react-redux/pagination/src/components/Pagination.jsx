f418ab2ca4ccaa1e2af11d837f9241349ac2356a
art-litv <artem15052003@gmail.com>
2022-01-22 17:10:53 +0200

    changed pagination

--- redux/react-redux/pagination/src/components/Pagination.jsx
+++ redux/react-redux/pagination/src/components/Pagination.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 function Pagination({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) {
-  const isFirstPage = currentPage === 0;
-  const isLastPage = currentPage === Math.ceil(totalItems / itemsPerPage) - 1;
+  const isPrevPageAvailable = currentPage === 0;
+  const isNextPageAvailable =
+    currentPage === Math.ceil(totalItems / itemsPerPage) - 1;
 
   return (
     <div className="pagination">
-      <button className="btn" onClick={goPrev} disabled={isFirstPage}>
-        {!isFirstPage && "←"}
+      <button className="btn" onClick={goPrev} disabled={isPrevPageAvailable}>
+        {!isPrevPageAvailable && "←"}
       </button>
       <span className="pagination__page">{currentPage + 1}</span>
-      <button className="btn" onClick={goNext} disabled={isLastPage}>
-        {!isLastPage && "→"}
+      <button className="btn" onClick={goNext} disabled={isNextPageAvailable}>
+        {!isNextPageAvailable && "→"}
       </button>
     </div>
   );
